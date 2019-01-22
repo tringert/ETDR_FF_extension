8f8059704c9ee3f6aae84b41f59aaa84015b9263
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2019-01-22 13:29:42 +0100

    The filename cell's value had been changed, so acquiring the exact filename needed to be modified
    Version bump to 1.3

--- content.js
+++ content.js
@@ -33,7 +33,7 @@ function getDownloadInfo(){
 
     for (i = 0; i < infos.length; i++) {
 
-        let item = text[i + 1].cells[fpos].innerHTML.replace(/<.*\/a>/, '').replace(/\s+$/, '').replace(regex, '');
+        let item = text[i + 1].children[3].firstChild.textContent.trim();
         infos[i].filename = item;
     }
 
