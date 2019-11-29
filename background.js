47b318317880b8dde921c899607225e34ff4cc75
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2019-11-29 19:46:39 +0100

    Ver 1.5.0 - Fix the problem when identical filenames wasn't renamed - Downloads now will be put in a folder which is named after the process number

--- background.js
+++ background.js
@@ -2,40 +2,51 @@ var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?etdr\.gov\.hu\/(RDProcessAction\/Proc
 var browserVersion = '';
 
 // A function to use as callback
-function doStuffWithDom(infos) {
+async function doStuffWithDom(jsonData) {
 
-	for (i = 0; i < infos.length; i++) { 
-		
-		var downloading = browser.downloads.download({
-			url : infos[i].link,
-			filename : infos[i].filename,
-			conflictAction : 'uniquify'
-		});
-	}
+    var infos = JSON.parse(jsonData);
+
+    // Set the folder name
+    var downloadPrefix = infos.processNumber === "" ? "" : infos.processNumber + "/";
+
+    // Iterate through elements and start the download
+    for (i = 0; i < infos.loc.length; i++) {
+
+        await dLoad(infos.loc[i].link, downloadPrefix + infos.loc[i].filename);
+    }
+}
+
+// Download method
+async function dLoad(url, fileName) {
+    var downloading = await browser.downloads.download({
+        url: url,
+        filename: fileName,
+        conflictAction: 'uniquify'
+    });
 }
 
 // A function to send back the browser's version
 function setBrowserVersion(browserInfo) {
-		browserVersion = browserInfo.version.slice(0, 2);
-	}
+    browserVersion = browserInfo.version.slice(0, 2);
+}
 
 // When the browser-action button is clicked...
 browser.browserAction.onClicked.addListener(async function (tab) {
-    
-	// Check the browser's version if this is a working version.
-	// From version 67 to 69 the API's download() function won't include cookies in the requests (https://bugzilla.mozilla.org/show_bug.cgi?id=1555591)
-	var gettingInfo = browser.runtime.getBrowserInfo();
-	
-	await gettingInfo.then(setBrowserVersion);
-	
-	if (browserVersion === '67' || browserVersion === '68') {
-		chrome.tabs.sendMessage(tab.id, {text: 'not_supported_browser_version'}, doStuffWithDom);
-		return;
-	}
-	
-	// ...check the URL of the active tab against our pattern and...
+
+    // Check the browser's version if this is a working version.
+    // From version 67 to 69 the API's download() function won't include cookies in the requests (https://bugzilla.mozilla.org/show_bug.cgi?id=1555591)
+    var gettingInfo = browser.runtime.getBrowserInfo();
+
+    await gettingInfo.then(setBrowserVersion);
+
+    if (browserVersion === '67' || browserVersion === '68') {
+        chrome.tabs.sendMessage(tab.id, { text: 'not_supported_browser_version' }, doStuffWithDom);
+        return;
+    }
+
+    // ...check the URL of the active tab against our pattern and...
     if (urlRegex.test(tab.url)) {
         // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+        chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, doStuffWithDom);
     }
 });
--- content.js
+++ content.js
@@ -1,34 +1,44 @@
 // Listen for messages
 browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    
-	// If the received message has the expected format...
+
+    // If the received message has the expected format...
     if (msg.text === 'report_back') {
         // ... call the specified callback, passing
         // the data back to the background page
         sendResponse(getDownloadInfo());
     }
-	
-	if (msg.text === 'not_supported_browser_version') {
-		alert("A Firefox 67-es verziójától a 69-es verziójáig nem működik ez a kiegészítő, mert ezekben a verziókban a letöltéshez szükséges funkció hibás.");
-	}
+
+    if (msg.text === 'not_supported_browser_version') {
+        alert("A Firefox 67-es verziójától a 69-es verziójáig nem működik ez a kiegészítő, mert ezekben a verziókban a letöltéshez szükséges funkció hibás.");
+    }
 });
 
 // Get the info for download
-function getDownloadInfo(){
-	
-    var links = document.querySelectorAll('#uploadedDocs_DXMainTable a');
-    var infos = [];
-	var regex = new RegExp(/[\x00-\x1F\x7F-\x9F]/gu);
+function getDownloadInfo() {
 
+    var links = document.querySelectorAll('#uploadedDocs_DXMainTable a');
+    var loc = [];
+    var regex = new RegExp(/[\x00-\x1F\x7F-\x9F]/gu);
+    var devUrlPrefix;
+
+    // Set the url prefix
+    if (window.location.href.match(/fejl\.etdr\.gov\.hu/)) {
+        devUrlPrefix = "https://fejl.etdr.gov.hu";
+    } else {
+        devUrlPrefix = "https://www.etdr.gov.hu";
+    }
+
+    // Get hrefs and make the links
     for (link of links) {
         let href = link.getAttribute('href');
         if (href.match(/\/Document\/Download\//) || href.match(/\/RDDocument\/Download\//)) {
-			infos.push({ link: `https://www.etdr.gov.hu${href}` });
-		}
+            loc.push({ link: `${devUrlPrefix + href}` });
+        }
     }
 
     var text = document.querySelectorAll('#uploadedDocs_DXMainTable tr');
-    // Filename position in table
+
+    // Filename position in table (it varies between the tables)
     var fpos = 1;
     if (window.location.href.match(/etdr\.gov\.hu\/RDProcessByUser\/ProcessEdit/) ||
         window.location.href.match(/etdr\.gov\.hu\/ProcessByOffice\/ProcessEdit/) ||
@@ -36,11 +46,19 @@ function getDownloadInfo(){
         fpos = 3;
     }
 
-    for (i = 0; i < infos.length; i++) {
+    // Get the filenames
+    for (i = 0; i < loc.length; i++) {
 
         let item = text[i + 1].children[fpos].firstChild.textContent.trim();
-        infos[i].filename = item;
+        loc[i].filename = item;
     }
 
-    return infos;
+    // Get the process number
+    var procinfoSection = document.querySelector("#procinfo-section");
+    var procNum = procinfoSection == null ? "" : procinfoSection.dataset.processnumber;
+
+    // Generate the object
+    var infosObject = JSON.stringify({ processNumber: procNum, loc });
+
+    return infosObject;
 }
