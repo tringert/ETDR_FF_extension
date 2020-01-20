849734fc5c4155ad0f50fa8a7fefbf966a8c645c
Tringer Tamás <tamas.tringer@lechnerkozpont.hu>
2020-01-20 13:54:05 +0100

    1. getMonth() in Date object gives back 0 for January in Javascript 2. Bump version number to 1.5.3

--- background.js
+++ background.js
@@ -75,7 +75,7 @@ async function dLoad(url, fileName) {
 function currentDateTimeAsFolderName() {
     var dt = new Date();
     var year = ('0' + dt.getFullYear().toString()).slice(-4);
-    var month = ('0' + dt.getMonth().toString()).slice(-2);
+    var month = ('0' + (dt.getMonth() + 1).toString()).slice(-2);
     var day = ('0' + dt.getDay().toString()).slice(-2);
     var hour = ('0' + dt.getHours().toString()).slice(-2);
     var minutes = ('0' + dt.getMinutes().toString()).slice(-2);
